6b124fa27034cc6c04ce1e4b6c79d4951ad29c03
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2021-07-19 16:26:01 +0800

    safe_atom增加函数

--- src/useful_tools/safe_atom/safe_atom.erl
+++ src/useful_tools/safe_atom/safe_atom.erl
@@ -1,9 +1,22 @@
 -module(safe_atom).
 
--export([list_to_atom/1]).
+-export([list_to_atom/1, binary_to_atom/1, binary_to_term/1]).
 
 list_to_atom(List) when is_list(List) ->
-  case catch (list_to_existing_atom(List)) of
-    {'EXIT', _} -> erlang:list_to_atom(List);
-    Atom when is_atom(Atom) -> Atom
+  case catch list_to_existing_atom(List) of
+    {'EXIT', _} ->
+      erlang:list_to_atom(List);
+    Atom when is_atom(Atom) ->
+      Atom
   end.
+
+binary_to_atom(Bin) when is_binary(Bin) ->
+  case catch binary_to_existing_atom(Bin) of
+    {'EXIT', _} ->
+      erlang:binary_to_atom(Bin);
+    Atom when is_atom(Atom) ->
+      Atom
+  end.
+
+binary_to_term (Bin) ->
+  erlang:binary_to_term (Bin, [safe]).
