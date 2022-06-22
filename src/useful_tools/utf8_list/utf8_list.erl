fe8e8719a15cb1a55db1a108096d4320cdb1b627
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2022-06-22 10:32:59 +0800

    更新

--- src/useful_tools/utf8_list/utf8_list.erl
+++ src/useful_tools/utf8_list/utf8_list.erl
@@ -1,6 +1,9 @@
 -module(utf8_list).
 
--export([list_to_binary/1]).
+-export([list_to_binary/1, binary_to_list/1]).
 
 list_to_binary(List) ->
   unicode:characters_to_binary(List).
+
+binary_to_list(InfoBin) ->
+  unicode:characters_to_list(InfoBin, utf8).
