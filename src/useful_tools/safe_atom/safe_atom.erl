b9248bcfef27878de26a7af4cdb3941ccd4de43a
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-11-14 11:55:55 +0800

    .

--- src/useful_tools/safe_atom/safe_atom.erl
+++ src/useful_tools/safe_atom/safe_atom.erl
@@ -3,20 +3,22 @@
 -export([list_to_atom/1, binary_to_atom/1, binary_to_term/1]).
 
 list_to_atom(List) when is_list(List) ->
-  case catch list_to_existing_atom(List) of
-    {'EXIT', _} ->
-      erlang:list_to_atom(List);
-    Atom when is_atom(Atom) ->
-      Atom
-  end.
+    try list_to_existing_atom(List) of
+        Atom when is_atom(Atom) ->
+            Atom
+    catch
+        _:_Reason ->
+            erlang:list_to_atom(List)
+    end.
 
 binary_to_atom(Bin) when is_binary(Bin) ->
-  case catch binary_to_existing_atom(Bin) of
-    {'EXIT', _} ->
-      erlang:binary_to_atom(Bin);
-    Atom when is_atom(Atom) ->
-      Atom
-  end.
+    try binary_to_existing_atom(Bin) of
+        Atom when is_atom(Atom) ->
+            Atom
+    catch
+        _:_Reason ->
+            erlang:binary_to_atom(Bin, utf8)
+    end.
 
-binary_to_term (Bin) ->
-  erlang:binary_to_term (Bin, [safe]).
+binary_to_term(Bin) ->
+    erlang:binary_to_term(Bin, [safe]).
